9b10fa1351e230d258e89075ac570dd09f679c53
James Hollingshead <git@jameshollingshead.com>
2015-11-03 14:49:21 -0500

    First Commit
    Created project, added test harness, added project requirements document

---
+++ VendingMachineKata/VendingMachineKataTest/VendingMachineKataTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit;
+using VendingMachineKata;
+
+namespace VendingMachineKataTest
+{
+    [TestFixture]
+    public class TestClass
+    {
+        VendingMachine vendingMachine;
+
+
+        [SetUp]
+        public void TestSetup()
+        {
+            vendingMachine = new VendingMachine();
+        }
+
+    }
+}
